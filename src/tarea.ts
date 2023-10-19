60eefa2da9a631a38c4bcdd902dac914e91d5991
JaviGP2002 <javigp2002@gmail.com>
2023-10-19 13:30:33 +0200

    Solucionado #10 y #15 closes #10 #15
    Se ha creado la estructura de una tarea y no existen tareas predeterminadas

--- src/tarea.ts
+++ src/tarea.ts
@@ -10,5 +10,18 @@ enum Prioridad{
     Baja
 }
 
+export class Tarea{
+    private _titulo: string;
+    private _tiempoEstimado: number;
+    private _fechaCreacion: Date;
+    private _prioridad: Prioridad
+    
 
-
+    constructor(titulo:string, tiempoEstimado:number, fechaCreacion:Date, prioridad:Prioridad){
+        this._titulo = titulo;
+        this._tiempoEstimado = tiempoEstimado;
+        this._fechaCreacion = fechaCreacion;
+        this._prioridad = prioridad;
+    }
+    
+}
