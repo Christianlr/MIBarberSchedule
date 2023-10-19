c2d448eaace7730e3fd1bcbba84b78788eeb8988
JaviGP2002 <javigp2002@gmail.com>
2023-10-19 13:36:33 +0200

    Solucionado #12 closes #12
    Este modelo permite asignar los tiempos libres y duraciones para asignarles tareas

---
+++ src/agenda.ts
@@ -0,0 +1,14 @@
+/**
+ * Clase Agenda
+ * Entidad
+ * Implementación de la clase Agenda que unirá los diferentes
+ * objetos valores de la aplicación
+ */
+
+
+type IntervaloTiempoLibreTrabajador ={
+    inicio: Date,
+    duracion: number
+    trabajador: string
+}
+
