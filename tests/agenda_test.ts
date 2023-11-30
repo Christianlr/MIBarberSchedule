5b6d36f16585aaaba553ddfd6fc49a0022c1bea2
Christianlr <e.chrlr@go.ugr.es>
2023-12-01 00:29:53 +0100

    Arreglo y adapto test; close #39 adapto test a #38 y añado prueba de ajuste de prioridad y de lógica óptima

--- tests/agenda_test.ts
+++ tests/agenda_test.ts
@@ -19,69 +19,86 @@ describe('Test Agenda', () => {
   let _agenda: Agenda;
   let _tiempoLibreTrabajadores: Array<IntervaloTiempoLibreTrabajador>;
   let _tareasAsignadas: Map<Tarea, IntervaloTiempoLibreTrabajador>;
-  let _barberosRegistrados: Array<string>;
   let _tareas: Array<Tarea>;
 
-  function extraerDatosJSON() {
-    if (datosBarberia.barberos && Array.isArray(datosBarberia.barberos)) {
-      rellenarBarberos();
-    }
+  function rellenarTareas() {
     if (datosBarberia.tareas && Array.isArray(datosBarberia.tareas)) {
-      rellenarTareas();
-    }
-    if (datosTiemposLibres.intervalos && Array.isArray(datosTiemposLibres.intervalos)) {
-      rellenarIntervalosLibres();
+
+      _tareas = datosBarberia.tareas.map((tareaData: {
+        titulo: string, tiempoEstimadoEnMinutos: number, prioridad: number, fechaCreacion: string
+      }) => {
+        const tarea = new Tarea(
+          tareaData.titulo,
+          tareaData.tiempoEstimadoEnMinutos,
+          tareaData.prioridad,
+          new Date(tareaData.fechaCreacion)
+        );
+        return tarea;
+      });
     }
   }
 
-  function rellenarBarberos() {
-    _barberosRegistrados = datosBarberia.barberos.map((barbero: { nombre: string }) => barbero.nombre);
-  }
-  function rellenarTareas() {
-    _tareas = datosBarberia.tareas.map((tareaData: {
-      titulo: string, tiempoEstimadoEnMinutos: number, prioridad: number, fechaCreacion: string
-    }) => {
-      const tarea = new Tarea(
-        tareaData.titulo,
-        tareaData.tiempoEstimadoEnMinutos,
-        tareaData.prioridad,
-        new Date(tareaData.fechaCreacion)
-      );
-      return tarea;
-    });
-  }
   function rellenarIntervalosLibres() {
-    _tiempoLibreTrabajadores = datosTiemposLibres.intervalos.map(item => {
-      return {
-        trabajador: item.trabajador,
-        intervalosLibres: item.intervalosLibres.map(intervalo => {
-          return {
-            inicio: new Date(intervalo.inicio),
-            duracionEnMinutos: intervalo.duracionEnMinutos
-          };
-        })
-      };
+    if (datosTiemposLibres.intervalos && Array.isArray(datosTiemposLibres.intervalos)) {
+      _tiempoLibreTrabajadores = datosTiemposLibres.intervalos.map(item => {
+        return {
+          trabajador: item.trabajador,
+          intervalosLibres: item.intervalosLibres.map(intervalo => {
+            return {
+              inicio: new Date(intervalo.inicio),
+              duracionEnMinutos: intervalo.duracionEnMinutos
+            };
+          })
+        };
+      });
+    }
+  }
+
+  function asignacionTareasNoOptimo(tiempoLibreTrabajadores: Array<IntervaloTiempoLibreTrabajador>, tareasAsignadas: Map<Tarea, IntervaloTiempoLibreTrabajador> , tareas: Array<Tarea>) {
+    tareas.forEach(tarea => {
+      if (!tareasAsignadas.has(tarea)) {
+        tiempoLibreTrabajadores.forEach(barbero => {
+          let intervalosLibres = barbero.intervalosLibres.filter(
+            intervalo => !tareasAsignadas.has(tarea) && intervalo.duracionEnMinutos >= tarea.tiempoEstimadoEnMinutos
+          );
+  
+          if (intervalosLibres.length > 0) {
+            let intervaloSeleccionado = intervalosLibres[0];
+            tareasAsignadas.set(tarea, { trabajador: barbero.trabajador, intervalosLibres: [intervaloSeleccionado] });
+  
+            tareas = tareas.filter(t => t !== tarea);
+  
+            const indexTrabajador = tiempoLibreTrabajadores.findIndex(item => item.trabajador === barbero.trabajador);
+            tiempoLibreTrabajadores[indexTrabajador].intervalosLibres = tiempoLibreTrabajadores[indexTrabajador].intervalosLibres.map(intervalo => {
+              if (intervalo === intervaloSeleccionado) {
+                return {
+                  inicio: intervalo.inicio,
+                  duracionEnMinutos: intervalo.duracionEnMinutos - tarea.tiempoEstimadoEnMinutos
+                };
+              }
+              return intervalo;
+            });
+            return;
+          }
+        });
+      }
     });
   }
 
   beforeEach(() => {
 
-    _barberosRegistrados = [];
     _tiempoLibreTrabajadores = [];
     _tareasAsignadas = new Map();
     _tareas = [];
-    _agenda = new Agenda();
-
-    extraerDatosJSON();
-    _agenda.asignacionTareas();
+    rellenarIntervalosLibres();
+    rellenarTareas();
+    _agenda = new Agenda(_tiempoLibreTrabajadores,_tareas, _tareasAsignadas);
 
   });
 
   test('Inicialización de variables agenda correctamente.', () => {
 
-    expect(_barberosRegistrados instanceof Array);
     expect(_tareas instanceof Array);
-    expect(_barberosRegistrados.length).toBeGreaterThan(0);
     expect(_tareas.length).toBeGreaterThan(0);
     expect(_tareasAsignadas instanceof Map).toBe(true);
     expect(_tiempoLibreTrabajadores).toBeInstanceOf(Array);
@@ -90,9 +107,12 @@ describe('Test Agenda', () => {
 
   test('Asignación de tareas a barberos', () => {
     expect(_tiempoLibreTrabajadores.length).toBeGreaterThan(0);
+    expect(_tareas.length).toBeGreaterThan(0);
+
+    _agenda.asignacionTareas();
+
+    expect(_tareasAsignadas.size).toBeGreaterThan(0);
     _tareasAsignadas.forEach((barbero, tarea) => {
-      expect(_tareas.includes(tarea)).toBe(false);
-      expect(barbero.intervalosLibres.some(intervalo => tarea.fechaCreacion >= intervalo.inicio)).toBe(true);
       let tareaAsignada = _tareasAsignadas.get(tarea);
       expect(tareaAsignada).toBeDefined();
       if (tareaAsignada) {
@@ -102,25 +122,36 @@ describe('Test Agenda', () => {
         expect(tiempoLibreSuficiente).toBe(true);
       }
     });
+    
+  });
 
-    let tareasRestantes = _tareas.length;
-    let tareasAsignadas = _tareas.length - tareasRestantes;
-    expect(_tareasAsignadas.size).toBe(tareasAsignadas);
-    expect(tareasRestantes).toBeLessThanOrEqual(_tareas.length);
+  test('La prioridad se ajusta correctamente.', ()=>{
+    
+    let _tareaAntigua = new Tarea('Tarea Antigua', 10, 2, new Date('2022-01-01'));
+    _agenda['_tareas'] = [_tareaAntigua];
+    expect(_tareaAntigua.prioridad).not.toBe(1);
+    _agenda.ajusteDePrioridad();
+    expect(_tareaAntigua.prioridad).toBe(1);
+  
   });
 
   test('La asignación de tareas es óptima en cuanto a reparto por prioridad.', () => {
-    //Se comprueba que la asignación de tareas a los barberos sea por orden de prioridad consultando sus asignaciones.
-    _tareasAsignadas.forEach((barbero, tarea) => {
-      const tareasAsignadasBarbero = Array.from(_tareasAsignadas.keys())
-        .filter(t => _tareasAsignadas.get(t)?.trabajador === barbero.trabajador)
-        .sort((a, b) => a.prioridad - b.prioridad);
-      if (tareasAsignadasBarbero.length > 1){
-        for (let i = 0; i < tareasAsignadasBarbero.length - 1; i++) {
-          expect(tareasAsignadasBarbero[i].prioridad).toBeLessThanOrEqual(tareasAsignadasBarbero[i + 1].prioridad);
-        }
-      }
-    });
-  });
+    
+    let _tiempoLibreTrabajadoresNoOptimo = _tiempoLibreTrabajadores.slice();
+    let _tareasAsignadasNoOptimo = new Map(_tareasAsignadas);
+    let _tareasNoOptimo = _tareas.slice();
+
+    asignacionTareasNoOptimo(_tiempoLibreTrabajadoresNoOptimo, _tareasAsignadasNoOptimo, _tareasNoOptimo);
+    
+    _agenda.asignacionTareas();
+
+    const tareasAsignadasOriginal = Array.from(_tareasAsignadas.keys());
+    const tareasAsignadasNoOptimo = Array.from(_tareasAsignadasNoOptimo.keys());
   
+    tareasAsignadasOriginal.forEach((tarea, i) => {
+      expect(tarea.prioridad).toBeLessThanOrEqual(tareasAsignadasNoOptimo[i].prioridad);
+    });
+    
+  });
+
 });
